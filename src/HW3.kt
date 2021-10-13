f52a5fa980d79bc2b2ca5d4eef11f44a39f0c92f
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2021-10-13 22:27:51 +0300

    add hw3 q1

--- src/HW3.kt
+++ src/HW3.kt
@@ -1,11 +1,23 @@
 
 fun main() {
+
      // Q1: chapter 3
+    val name = "Madrigal"
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20 ).toInt()
 
-
+    when{
+        karma in 0 .. 5 -> println("red")
+        karma in 6 .. 10 -> println("orange")
+        karma in 11 .. 15 -> println("purple")
+        karma in 16 .. 20 -> println("green")
+        else -> println("green")
+    } // end when
 
 
     // Q2: create non return value fun
     printInfo()
 
-}
+} // end main
