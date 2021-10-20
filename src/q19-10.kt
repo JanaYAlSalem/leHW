8bdfb29cc2442b55c05606286c03995d143cb9a8
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2021-10-20 07:55:57 +0300

    answer of bonus question

---
+++ src/q19-10.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+ var ChUser = 5
+
+
+for (CountA in 1 .. ChUser){
+    for (CountB in 1 .. CountA){
+        print(" ($CountB*$ChUser) ")
+    } // end b
+    println()
+} // end for1
+
+
+} // end main
